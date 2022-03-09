709d04b87d7a81ed5f39a0c1256e0eb16a451e55
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-03-10 00:03:32 +0700

    Update sewa.js

--- plugins/sewa.js
+++ plugins/sewa.js
@@ -1,82 +1,40 @@
 let fetch = require('node-fetch')
-
 let handler = async (m, { conn }) => {
-
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╭═══ *〘 SEWA 〙*
-
 ║ ┅ ๑————————————๑
-
 ║┊ ⌲ Sewa = _5k/grup (1 minggu)_
-
-║┊ ⌲ Sewa = _15k/grup (1 bulan)_
-
-║┊ ⌲ Sewa = _20k/grup (2 bulan)_
-
-║┊ ⌲ Sewa = _35k/grup (8 bulan)_                                                                      ║┊ ⌲ Sewa = _45k/grup (12 bulan)_
-
-╰═ ┅ ═══════
-
+║┊ ⌲ Sewa = _10k/grup (1 bulan)_
+║┊ ⌲ Sewa = _20k/grup (Permanen)_                                                                    ╰═ ┅ ═══════
 ╭═══ *〘 BELI PREMIUM 〙*
-
 ║ ┅ ๑————————————๑
-
-║┊ ⌲ Premium = _10k/minggu_
-
-║┊ ⌲ Premium = _35k/bulan_
-
-║┊ ⌲ Premium = _46k/permanen
-
-║┊ ⌲ Jadi Owner = _75k/permanen
-
+║┊ ⌲ Premium = _5k/minggu_
+║┊ ⌲ Premium = _20k/bulan_
+║┊ ⌲ Premium = _30k/permanen_
 ╰═ ┅ ═══════
-
-
 
 ༅ _*KEUNTUNGAN SEWABOT*_
 
-
-
 ✧ *Bot akan masuk gc*
-
 ✧ *Bisa Menyuruh Bot Buka/Tutup Group*
-
 ✧ *Kick Orang*
-
 ✧ *Add Orang*
-
 ✧ *Anti Link On*
-
 ✧ *Bisa Memasukkan Bot Ke dalam Grup*
-
-
+✧ *Membuat sticker*
+✧ *Mendowload lagu*
 
 *[❗] Jika Group Sepi 2 minggu, bot akan leave*
 
-
-
 ◪ *Jika Minat Hubungi owner kami*
-
 ◪ *Atau Masuk Group Official Bot*
-
 _https://chat.whatsapp.com/DKfsblwOCTRBa9rrAmI7JA_
 
-
-
 ▌│█║▌║▌║║▌║▌║█│▌
-
-`.trim(), `AlpinXD`, 'Payment', '#payment', 'Owner', '#owner', m)
-
+`.trim(), `Alpin-XD`, 'Payment', '#payment', 'Owner', '#owner', m)
 }
 
-
-
 handler.help = ['sewa', 'sewabot']
-
 handler.tags = ['main']
-
 handler.command = /^sewa(bot)?$/i
 
-
-
 module.exports = handler
-

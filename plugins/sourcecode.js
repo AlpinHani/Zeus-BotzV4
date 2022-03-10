070c0f47b767840513a8c3cc2e576eb0505d30f8
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-03-10 23:32:51 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 *────────「 SCRIPT 」 ────────*
-_*Script?, Private Banh*_
+_*SCRIPT?, PRIVAT DEK :V*_
 `.trim(), '© Alpin-XD', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']

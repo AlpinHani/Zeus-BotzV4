71d5756c7dc994fa03bc000dd23c25efc05d4103
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-03-10 23:26:02 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
     "displayName": "Owner Gw",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Owner AlpinXD\nitem1.TEL;waid=6282287750102:6282287750102\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:alpinuwu@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hmm.com\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia\nitem5.X-ABLabel:───────[ ALPIN-XD ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Owner Alpin-XD\nitem1.TEL;waid=6282287750102:6282287750102\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:alpinuwu@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hmm.com\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia\nitem5.X-ABLabel:───────[ ALPIN-XD ]───────\nEND:VCARD"
   }, {
     "displayName": "Whatsapp",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"

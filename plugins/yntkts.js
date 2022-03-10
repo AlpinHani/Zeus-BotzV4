5a7f9fa5e75d7b096b4b019427e99994a74d786d
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-03-10 23:30:06 +0700

    Update yntkts.js

--- plugins/yntkts.js
+++ plugins/yntkts.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /(Yntkts|tahu)/
+handler.customPrefix = /(Yntkts|tau)/
 handler.command = new RegExp
 module.exports = handler
